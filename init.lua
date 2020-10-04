9021e4bbc47ca15b349127b460598a753bf5cdd0
Yiu Man Ho <yiufamily.hh@gmail.com>
2020-10-04 17:08:11 +0800

    Publish

--- init.lua
+++ init.lua
@@ -1,8 +1,10 @@
-local function start_day_vote(name, param)
-	
-
+vote_time = {}
+function vote_time.start_vote(name, param, time, description, sd)
+	if not(type(time) == "number") then
+		error("Vote Time Function: Time not number")
+	end
 	vote.new_vote(name, {
-		description = "Make day " .. param,
+		description = description,
 		help = "/yes,  /no  or  /abstain",
 		name = param,
 		duration = 60,
@@ -10,8 +12,8 @@ local function start_day_vote(name, param)
 		on_result = function(self, result, results)
 			if result == "yes" then
 				minetest.chat_send_all("Vote passed, " ..
-                        #results.yes .. " to " .. #results.no .. ", Time will be set to day ")
-                        minetest.set_timeofday(0.5)
+                        #results.yes .. " to " .. #results.no .. ", Time will be set to "..sd)
+                        minetest.set_timeofday(time)
 				
 			else
 				minetest.chat_send_all("Vote failed, " ..
@@ -32,5 +34,16 @@ minetest.register_chatcommand("vote_day", {
 	privs = {
 		interact = true
 	},
-	func = start_day_vote
+	func = function(name, param)
+		vote_time.start_vote(name, param, 0.5, "Make day "..param, "day ")
+	end
+})
+
+minetest.register_chatcommand("vote_night", {
+	privs = {
+		interact = true
+	},
+	func = function(name, param)
+		vote_time.start_vote(name, param, 0, "Make night "..param, "night ")
+	end
 })
